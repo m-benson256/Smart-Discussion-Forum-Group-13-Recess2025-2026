3fd118f9495e1c613436cd7f08fa75c39951af2d
Mwesigye Benson <www.mwesigyeb2022@gmail.com>
2026-07-02 11:25:47 +0300

    Create warnings table with necessary fields
    Added columns for WarningID, WarningNumber, UserID, IssuedBy, IssuedAt, Deadline, and Status to the warnings table.

--- database/migrations/2026_06_30_150122_create_warnings_table.php
+++ database/migrations/2026_06_30_150122_create_warnings_table.php
@@ -12,7 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('warnings', function (Blueprint $table) {
-            $table->id();
+            $table->id('WarningID'); // PK [cite: 27]
+            $table->integer('WarningNumber'); [cite: 27]
+            $table->foreignId('UserID')->constrained('members', 'UserID')->onDelete('cascade'); // Given to Member [cite: 27, 35]
+            $table->foreignId('IssuedBy')->constrained('administrators', 'AdminID')->onDelete('cascade'); // Issued by Admin [cite: 27, 35]
+            $table->timestamp('IssuedAt')->useCurrent(); [cite: 27]
+            $table->dateTime('Deadline'); [cite: 27]
+            $table->enum('Status', ['pending', 'resolved', 'expired'])->default('pending'); [cite: 27]
             $table->timestamps();
         });
     }
