ab451aaebe7c72236fdf1fdd62da0c92feac4f50
Jemimah-A <jemimahamutuhaire1@gmail.com>
2026-07-20 22:38:10 +0300

    check inactivity

--- app/Models/GroupMember.php
+++ app/Models/GroupMember.php
@@ -8,9 +8,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class GroupMember extends Model
 {
     protected $fillable = [
-        'user_id',
-        'group_id',
-    ];
+    'user_id',
+    'group_id',
+    'inactivity_warning_count',
+    'last_warned_at',
+    'status',
+    'blacklisted_until',
+];
 
     public function user(): BelongsTo
     {
