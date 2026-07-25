631a594f8b0879dcb06243cc246ec7ca5e07242a
m-benson256 <www.mwesigyeb2022@gmail.com>
2026-07-25 14:08:49 +0300

    deploying

--- database/migrations/2026_07_02_135828_add_columns_to_topics_table.php
+++ database/migrations/2026_07_02_135828_add_columns_to_topics_table.php
@@ -1,51 +1,35 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
+public function up(): void
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('Topics', function (Blueprint $table) {
+    Schema::table('Topics', function (Blueprint $table) {
+        if (!Schema::hasColumn('Topics', 'user_id')) {
             $table->foreignId('user_id')
                 ->after('id')
                 ->constrained('users')
                 ->cascadeOnDelete();
+        }
 
+        if (!Schema::hasColumn('Topics', 'group_id')) {
             $table->foreignId('group_id')
                 ->nullable()
                 ->after('user_id')
                 ->constrained('groups')
                 ->nullOnDelete();
+        }
 
+        if (!Schema::hasColumn('Topics', 'category_id')) {
             $table->foreignId('category_id')
                 ->nullable()
                 ->after('group_id')
                 ->constrained('categories', 'CategoryID')
                 ->nullOnDelete();
+        }
 
+        if (!Schema::hasColumn('Topics', 'title')) {
             $table->string('title');
+        }
+
+        if (!Schema::hasColumn('Topics', 'content')) {
             $table->text('content');
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('Topics', function (Blueprint $table) {
-            //
-            $table->dropForeign(['user_id']);
-            $table->dropForeign(['group_id']);
-            $table->dropForeign(['category_id']);
-            $table->dropColumn(['user_id', 'group_id', 'category_id', 'title', 'content']);
-
-        });
-    }
-};
+        }
+    });
+}
