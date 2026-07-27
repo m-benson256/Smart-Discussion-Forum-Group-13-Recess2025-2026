6927df992a49fd0de35f8ca1470b645c4d578595
m-benson256 <www.mwesigyeb2022@gmail.com>
2026-07-27 13:59:09 +0300

    debug announcements

--- app/Models/Announcements.php
+++ app/Models/Announcements.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Announcements extends Model
 {
+    protected $primaryKey = 'ID';
+
     protected $table = 'announcements'; // explicit, avoids any pluralization guessing issues
 
     protected $fillable = ['user_id', 'quiz_id', 'content','recipient_id'];
